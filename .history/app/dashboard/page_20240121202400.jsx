9d551fc8b67ea381c51b5c583f2ee789a4bf6039
DevLaukey <devlaukeymwaura@gmail.com>
2024-01-21 20:35:41 +0300

    added the dashboard successfully

--- .history/app/dashboard/page_20240121202400.jsx
+++ .history/app/dashboard/page_20240121202400.jsx
@@ -22,11 +22,8 @@ import {
 import { useRouter } from "next/navigation";
 
 import { ScrollArea } from "../../components/ui/scroll-area";
-import { CalendarDateRangePicker } from "../../components/date-range-picker";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../components/ui/tabs";
-import { Overview } from "../../components/overview";
 
-import { RecentSales } from "../../components/recent-sales";
+
 const DashboardPage = () => {
   const users = [
     { id: 1, name: "John Doe" },
@@ -41,7 +38,7 @@ const DashboardPage = () => {
   };
 
   return (
-    <ScrollArea className="h-full mt-8">
+    <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
--- .history/app/dashboard/page_20240121203204.jsx
+++ .history/app/dashboard/page_20240121203204.jsx
@@ -41,7 +41,7 @@ const DashboardPage = () => {
   };
 
   return (
-    <ScrollArea className="h-full mt-8">
+    <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
