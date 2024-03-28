1aa0dccd248178f3244e05e67c0a235f032079c3
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2024-03-28 09:28:33 +0300

    added responsiveness to the hero section

--- .history/components/landing/Hero_20240316110944.jsx
+++ .history/components/landing/Hero_20240316110944.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../ui/button";
 import { buttonVariants } from "../ui/button";
 import { HeroCards } from "./HeroCards";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
@@ -5,38 +6,39 @@ import Link from "next/link";
 
 export const Hero = () => {
   return (
-    <section className="container grid grid-cols-1 md:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-      <div className="text-center md:text-left space-y-6">
-        <main className="text-3xl md:text-5xl font-bold">
-          <h1 className="inline-block">
-            <span className="inline bg-gradient-to-r from-[#F596D3] to-[#D247BF] text-transparent bg-clip-text">
+    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+      <div className="text-center lg:text-start space-y-6">
+        <main className="text-5xl md:text-6xl font-bold">
+          <h1 className="inline">
+            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               P.O.E.M.S
-            </span>{" "}
+            </span>
             Point
-          </h1>{" "}
-          Of{" "}
-          <h2 className="inline-block">
+          </h1>
+          Of
+          <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
               Entry Management
-            </span>{" "}
+            </span>
             System
           </h2>
         </main>
 
-        <p className="text-lg text-muted-foreground md:w-10/12 mx-auto">
+        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
           An All in One Entry and Exit Management Solution
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
+         
           <button
             type="button"
-            className={`flex items-center justify-center w-full md:w-auto mt-3 text-white bg-black rounded-lg h-14 md:h-16 px-6 ${buttonVariants(
+            className={`flex items-center justify-center w-48 mt-3 text-white bg-black rounded-lg h-14 md:w-1/3 ${buttonVariants(
               {
                 variant: "outline",
               }
             )}`}
           >
-            <div className="mr-3">
+            <div class="mr-3">
               <svg viewBox="30 336.7 120.9 129.2" width="30">
                 <path
                   fill="#FFD400"
@@ -57,8 +59,8 @@ export const Hero = () => {
               </svg>
             </div>
             <div>
-              <div className="text-xs">GET IT ON</div>
-              <div className="-mt-1 font-sans text-base md:text-xl font-semibold">
+              <div class="text-xs">GET IT ON</div>
+              <div class="-mt-1 font-sans text-xl font-semibold">
                 Google Play
               </div>
             </div>
